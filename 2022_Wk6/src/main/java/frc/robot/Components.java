c999a00076d17e8a1d428057d28d6214449a27d3
redraider6381 <6381programming@gmail.com>
2022-02-17 17:56:00 -0600

    n
     nm

--- 2022_Wk6/src/main/java/frc/robot/Components.java
+++ 2022_Wk6/src/main/java/frc/robot/Components.java
@@ -2,6 +2,10 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class Components {
@@ -9,11 +13,11 @@ public class Components {
     public static CANSparkMax CANBackRight    = new CANSparkMax(12, MotorType.kBrushless);
     public static CANSparkMax CANFrontLeft    = new CANSparkMax(6, MotorType.kBrushless);
     public static CANSparkMax CANFrontRight   = new CANSparkMax(14, MotorType.kBrushless);
-
     public static XboxController XBController= new XboxController(0);
-
+    public static DoubleSolenoid intakePneumatic = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    public static Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
     public static CANSparkMax Indexer1    = new CANSparkMax(5, MotorType.kBrushless);
     public static CANSparkMax Indexer2   = new CANSparkMax(13, MotorType.kBrushless);
-    
-
+    public static Joystick happyStick = new Joystick(1);
+    public static CANSparkMax intakeMotor = new CANSparkMax(7, MotorType.kBrushless);
 }
